f84db550ca5134255f5125081673b1a5c8d28de0
bhupeshchandra07 <bhupeshchandra07@gmail.com>
2025-09-16 05:34:01 +0530

    added to Basics

---
+++ Basics/variableskikahani.js
@@ -0,0 +1,21 @@
+// to declare the variable in tha js we use the following rules and keywords in the js.
+const pi = 3.14 // can not be chagne if one defined
+
+let userName = "Bhupesh"//can be change when we need
+
+var userEmail = "bhupesh@gmail.com"//also can be change when we need
+
+age = 21//this is a also a way to define the variable but this is not a good pratice so dont use.
+
+let city;//this is also a way. but untill not get value it is undefined.
+
+//note: we won't be use the 'var' further in our coding journy because the var is not block scoped and the let
+// is the block scoped. so we use the two way to decalre our variable in js. 'const' and 'let'
+
+console.log(pi); // we can print one by one or a another method is given below.
+
+// this is the tabluer method to print the variables.
+console.log([pi,userName,userEmail,age,city]);
+
+
+
