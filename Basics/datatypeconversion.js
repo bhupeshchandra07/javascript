ab67173e533dc6451ebddd73714eb004a7ef6440
bhupeshchandra07 <bhupeshchandra07@gmail.com>
2025-09-16 15:25:07 +0530

    some operatoin on it

---
+++ Basics/datatypeconversion.js
@@ -0,0 +1,68 @@
+let score = "Bhupesh"
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number(score)// this is the Number base class whcih converts the String to Number;
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+
+// "33" => 33
+// "33abc" => NaN
+//  true => 1; false => 0
+
+let isLoggedIn = "Amit"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+ console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "hitesh" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);//33
+console.log(typeof stringNumber);//string
+
+// Operations
+
+let value = 3
+let negValue = -value
+console.log(negValue);//-3
+
+console.log(2+2);//4
+console.log(2-2);//0
+console.log(2*2);//4
+console.log(2**3);//8
+console.log(2/3);//0.6666666666666666
+console.log(2%3);//2
+
+let str1 = "hello"
+let str2 = " bhupesh"
+
+let str3 = str1 + str2
+console.log(str3);//hello bhupesh
+
+console.log("1" + 2);//12
+console.log(1 + "2");//12
+console.log("1" + 2 + 2);//122
+console.log(1 + 2 + "2");//32
+
+console.log( (3 + 4) * 5 % 3);//execite from the left to right
+
+console.log(+true);//1
+console.log(+"");//0
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);//101
+
+// link to study
+// https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
